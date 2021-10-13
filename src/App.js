9af864ca1c988914555a991f5c7eefa66a73869f
piyushbhang <piyush@trodl.com>
2021-10-13 16:14:18 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { Staking } from './components/staking/Staking';
+import { Staking } from './component/staking/Staking';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
         <Staking></Staking>
       </header>
     </div>
