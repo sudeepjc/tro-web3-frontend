15d01115be9e5463c9d7fccee1670499e6fb87b9
piyushbhang <piyush@trodl.com>
2021-10-13 16:00:27 +0530

    push base project

---
+++ src/components/staking/Staking.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Staking extends Component {
+    render() {
+        return (
+            <div>
+                This is a staking component...
+            </div>
+        )
+    }
+}
